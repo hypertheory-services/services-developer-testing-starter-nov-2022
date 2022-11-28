d2aa53a8d52426c2b102439953fe51a862c03260
Jeff Gonzalez <jeff@hypertheory.com>
2022-11-28 16:37:12 -0500

    done with monday

--- courses/src/CoursesApi.IntegrationTests/CoursesResource/GettingCourses.cs
+++ courses/src/CoursesApi.IntegrationTests/CoursesResource/GettingCourses.cs
@@ -3,6 +3,7 @@
 using Alba;
 
 using CoursesApi.IntegrationTests.CoursesResource.Fixtures;
+using CoursesApi.Models;
 
 namespace CoursesApi.IntegrationTests.CoursesResource;
 
@@ -20,11 +21,19 @@ public class GettingCourses : IClassFixture<CoursesResourceFixture>
     {
         // when I do a GET /courses
         // I get a 200 OK Status Response
-        await _host.Scenario(api =>
+        var response = await _host.Scenario(api =>
         {
-            api.Get.Url("/coursexs");
+            api.Get.Url("/courses");
             api.StatusCodeShouldBeOk(); // 200 OK
         });
+
+        var entity = await response.ReadAsJsonAsync<CoursesResponseModel>();
+
+        entity = entity is not null ? entity : throw new ArgumentNullException(nameof(entity));
+
+        Assert.Equal(0, entity.NumberOfFrontendCourses);
+        Assert.Equal(0, entity.NumberOfBackendCourses);
+        Assert.Empty(entity.Courses);
 
     }
 
